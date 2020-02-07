e6747ccb6af26a02c5f338cb6af3ca83e31a7832
Lizzyfemme <lizzy_swann@hotmail.com>
2020-02-06 18:21:27 -0800

    Setup completed

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import "./App.css";
+import HomeBackground from "./components/home/HomeBackground";
+
+function App() {
+  return (
+    <div className="App">
+      <HomeBackground></HomeBackground>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/home/HomeBackground.js
@@ -0,0 +1,19 @@
+// import landing_image from "./leone-venter-unsplash.jpg";
+// import cell_image from "./landing_cell.jpg";
+import styled from "styled-components";
+
+const HomeBackground = styled.div`
+  height: 100vh;
+  width: 100vw;
+  background-color: green;
+`;
+export default HomeBackground;
+// @media screen and (max-width: 500px) {
+//   background-image: url(${cell_image});
+// }
+// background-position: center top;
+// object-fit: cover;
+// background-repeat: no-repeat;
+// background-size: cover;
+// background-attachment: fixed;
+// background-image: url(${landing_image});
