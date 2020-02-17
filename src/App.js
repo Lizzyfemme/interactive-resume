0e12a852bec0a2e70512032ce6c33e780ddbedbc
Lizzyfemme <lizzy_swann@hotmail.com>
2020-02-17 14:13:14 -0800

    font embeded

--- src/App.js
+++ src/App.js
@@ -3,7 +3,15 @@ import React from "react";
 import Main from "./Main";
 
 function App() {
-  return <Main></Main>;
+  return (
+    <Main>
+      <style>
+        @import
+        url('https://fonts.googleapis.com/css?family=Montserrat&display=swap');
+      </style>
+      <link rel="stylesheet" href="https://use.typekit.net/zym3rky.css"></link>
+    </Main>
+  );
 }
 
 export default App;
