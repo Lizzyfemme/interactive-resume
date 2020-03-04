66f3dcb61fe0834db40ca89fc7182c55d9346547
Lizzyfemme <lizzy_swann@hotmail.com>
2020-03-03 18:41:05 -0800

    public_html folder added

---
+++ public_html/precache-manifest.fd717ee195ea5e79dc2f0e853fbdab18.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a3b3eeaf0562eb743a18afc1f515352",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f667490e4dc92ab7fd6d",
+    "url": "/static/css/main.cfa83cab.chunk.css"
+  },
+  {
+    "revision": "bb369e789be22b8bab0a",
+    "url": "/static/js/2.49c19245.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.49c19245.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f667490e4dc92ab7fd6d",
+    "url": "/static/js/main.9f4e172e.chunk.js"
+  },
+  {
+    "revision": "19133a5d6c7be98533aa",
+    "url": "/static/js/runtime-main.107bb8ca.js"
+  },
+  {
+    "revision": "57e7af5ee9ac3493051bdce967f547da",
+    "url": "/static/media/contact_desktop.57e7af5e.png"
+  },
+  {
+    "revision": "f98379f503f6fdee7ae334deb74ab057",
+    "url": "/static/media/contact_guy_cell.f98379f5.png"
+  },
+  {
+    "revision": "f6dac0673adf349498caa7b2117c8ac9",
+    "url": "/static/media/contact_guy_desktop.f6dac067.png"
+  },
+  {
+    "revision": "3349fae32a25978d356367cc28b3b808",
+    "url": "/static/media/contact_guy_tablet.3349fae3.png"
+  },
+  {
+    "revision": "a409a7938dad642ff190d5187b90e930",
+    "url": "/static/media/email.a409a793.png"
+  },
+  {
+    "revision": "616fd14619b7561107cc8e659911a1fd",
+    "url": "/static/media/github.616fd146.png"
+  },
+  {
+    "revision": "b6878775dc764d4f057a9f51ea798d26",
+    "url": "/static/media/home.b6878775.png"
+  },
+  {
+    "revision": "14feb1a6d985848e039b9651c90b280c",
+    "url": "/static/media/leone-venter-unsplash-cell.14feb1a6.jpg"
+  },
+  {
+    "revision": "1b798e9894e02706d8932f21009f022c",
+    "url": "/static/media/leone-venter-unsplash-tablet.1b798e98.jpg"
+  },
+  {
+    "revision": "85b043fe82a7f262162693e75cf93f84",
+    "url": "/static/media/leone-venter-unsplash.85b043fe.jpg"
+  },
+  {
+    "revision": "9a305ba40c43987a097aeb8bd4cfdebd",
+    "url": "/static/media/linkedin.9a305ba4.png"
+  },
+  {
+    "revision": "7692b624e4372e05bcfd864bb15ec7d8",
+    "url": "/static/media/resume_desktop.7692b624.png"
+  }
+]);
